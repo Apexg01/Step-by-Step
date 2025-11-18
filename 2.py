02f3298cb7096ef07ca26386ab4eaf348d2b7bac
Apexg01 <apexg016@gmail.com>
2025-11-19 00:52:57 +0530

    2

---
+++ 2.py
@@ -0,0 +1,7 @@
+Name = input("Enter Your name:- ")
+Age = input("Enter Your Age:-")
+Email = input("Enter Your E-mail:-")
+
+print("Hello,",Name) 
+print("Your age is:",Age, "and your E-mail is:",Email)
+# if two or more inputs then , will seperate them 
