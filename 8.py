e431dde590db53f640f3f081e778d68967a2d1c1
Apexg01 <apexg016@gmail.com>
2025-11-19 23:25:22 +0530

    ...

---
+++ 8.py
@@ -0,0 +1,22 @@
+text = "ApexHere"
+print(len(text))
+print(text[0:4])
+print(text[4:])
+# 4: resembles starting from 5 th position to end 
+print("The seconf half")
+
+print(text[-4:])
+# -4: resembles last 4 characters
+print(text[-8:-4])
+# - shows piche se aage 
+print("The third half")
+
+print (text[:])
+# : resembles starting to end
+
+print(text[0:8:4])
+# ratio se hota hai for every 8 characters after start every 4th character will be printed
+
+
+
+
