5d47fbabb466334f064c71b774b9dd260941c1f8
Apexg01 <apexg016@gmail.com>
2025-11-20 20:55:55 +0530

    ...

---
+++ 3.py
@@ -0,0 +1,16 @@
+your_age = int(input("Enter your age: "))
+your_mother_age = int(input("Enter your mothers age:"))
+
+sum = your_age + your_mother_age
+print("The sum of your age and your mother's age is:", sum)
+
+# space nhi de sakte jisse denotte kar rhe hai 
+#int() daal ke fir input dalna hai 
+#for decimal use float() 
+
+x = float(input("Enter a decimal number: "))
+print("You entered:", x)
+print("Type of the entered value is:", type(x)) 
+
+print(x)
+print(type(x))
---
+++ 4.py
@@ -0,0 +1,3 @@
+x = float(input("Enter number:"))
+print(x)
+print(type(x))
