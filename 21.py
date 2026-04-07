1f6d832c21b8c07965041d614967e409fa8a25ae
Apexg01 <apexg016@gmail.com>
2026-04-07 22:23:10 +0530

    String Slicing

---
+++ 21.py
@@ -0,0 +1,26 @@
+a = "Arjun Chaurasia"
+name = "Arjun"
+print (a[0])
+print (a[6])
+
+b = '''A friend in need \n 
+is a friend indeed'''
+
+for c in b:
+    print (c)
+
+# for __ in variable:        helps in access the characters of the string one by one
+
+print(len(a))        
+
+print (a[0:7])
+
+# a[0:7] means that it will print the characters from index 0 to index 6 (7-1) and it will not include the character at index 7
+
+print (name[0:-3]) 
+
+# -3 will print the string while ignoring the last 3 (as "-3") characters of the string 
+
+#-x ko add karenge total number of characters se for eg: number of characters in my name is 5. So , 5-x 
+
+ 
