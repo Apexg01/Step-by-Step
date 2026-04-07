b9296770fb6dd48e3e27950fe5164a5b31a13744
Apexg01 <apexg016@gmail.com>
2026-04-07 23:29:20 +0530

    String Methods

--- 6.py
+++ 6.py
@@ -14,3 +14,34 @@ a = "Hi"
 b = "there"
 print(a + " " + b)
 # + " " + to give space , my observation " " ke andar space dedo 
+
+print (msg.split())
+#split splits the string into list 
+print (name. replace ("Apex" , "Arjun"))
+#replace replaces the string with the new string given with the help of "," 
+
+# .rstrip() removes the characters inserted in the brackets from the right end of the string not from left 
+
+#.capitalize() converts the first character of the string to uppercase and the rest to lowercase, in short it corrects the grammmar in respect to capital letters and non capital letters
+
+print(name.center(90))
+
+# .center() alligns the string to the center as per the number given in the brackets and gives space on both sides of the string to make it in the center
+
+# .count() counts the number of times a specific character appears in the string and returns that count as an integer.
+
+# .endswith() checks if the string ends with a specific character and returns True if it does, otherwise it returns False.
+# with ("x", 4,10) u can specify the range for checking the endswith 
+
+# .find() searches for a specific character or substring in the string and returns the index number of the first occurrence. If the character or substring is not found, it returns -1.
+
+# "find" is used for finding an uncertain character while "index" is used for finding a certain character
+ 
+# .islower checks if the characters are in lowercase or not 
+
+# .isspace() checks if there are space given or not 
+
+# .isupper() and .startswith() are the opposite of the above mentioned 
+
+# .title() converts the first character of each word to uppercase and the rest to lowercase.
+
